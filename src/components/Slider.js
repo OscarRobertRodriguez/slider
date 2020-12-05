c6f5f37300160e103143490d5216ff5fbb8b913f
Oscar Rodriguez <oscarrobertrodriguez@gmail.com>
2020-12-04 18:40:54 -0600

    add images and startup files

---
+++ src/components/Slider.js
@@ -0,0 +1,16 @@
+
+
+const Slider = () => {
+	return (
+			<ul className='slider'>
+				<div>back</div>
+				<li>widow</li>
+				<li>window</li>
+				<li>window</li>
+				<li>window</li>
+				<div>next</div>
+			</ul>
+	)
+}
+
+export default Slider;
---
+++ src/data.js
@@ -0,0 +1,31 @@
+
+import blackCat from './imgs/blackCat.jpg';
+import blackCamera from './imgs/blackCamera.jpg';
+import blackFlower from './imgs/blackFlower.jpg';
+import blackBird from './imgs/blackbird.jpg';
+
+
+
+const imageData = () => {
+
+	return [
+		{
+			title: 'black cat',
+			src: blackCat
+		},
+		{
+			title: 'black camera',
+			src: blackCamera
+		},
+		{
+			title: 'black bird',
+			src:  blackBird
+		},
+		{
+			title: 'black flower',
+			src: blackFlower
+		}
+	]
+};
+
+export default imageData();
